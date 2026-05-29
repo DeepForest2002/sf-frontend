4e2f9198115e579a299197727c078ba9d39004ed
Sayan Pramanik <pramaniksayan123@gmail.com>
2026-05-29 13:04:07 +0530

    Update the backend urls second time

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 axios.defaults.withCredentials = true;
-const API = "https://sf-backend-8xuu.onrender.com/";
+const API = "https://sf-backend-8xuu.onrender.com";
 
 function App() {
   const [rules, setRules] = useState([]);
